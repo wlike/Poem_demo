dc8af8fbafc045984462c4e55346a290746f422c
johnxuan <johnxuan@xverse.cn>
2021-09-23 17:59:30 +0800

    add logging functionality

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, render_template, abort, jsonify
 from jiuge_lvshi import Poem
 import json
+import logging
 from utils import *
 
 model_path = 'model_jl/epoch=0-step=49999.ckpt'
@@ -33,6 +34,8 @@ def generate():
             else:  # 律诗
                 if len(prefix) != 8:
                     return json.dumps({"code": 402, "content": "你选择了{}，它的藏头词只能是8个汉字".format(genre2name[genre])}, ensure_ascii=False).encode("utf-8")
+        msg = "genre: {}, prefix: {}".format(genre2name[genre], prefix if prefix is not None else "")
+        app.logger.info(msg)
         content = poem.generate(genre=genre, prefix=prefix)
         sents = content[:-1].split("，")
         return json.dumps({"code": 200, "content": sents, "prefix": prefix}, ensure_ascii=False).encode("utf-8")
@@ -56,11 +59,21 @@ def generate():
                 if len(prefix) != 8:
                     res = "你选择了{}，它的藏头词只能是8个汉字".format(genre2name[genre])
                     return render_template("index.html", poem=res)
+        msg = "genre: {}, prefix: {}".format(genre2name[genre], prefix if prefix is not None else "")
+        app.logger.info(msg)
         content = poem.generate(genre=genre, prefix=prefix)
         res = "\n".join(content[:-1].split("，"))
         return render_template("index.html", prefix=prefix, poem=res)
 
 if __name__ == "__main__":
+    handler = logging.FileHandler("poem.log", encoding="UTF-8")
+    handler.setLevel(logging.DEBUG)
+    logging_format = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s")
+    handler.setFormatter(logging_format)
+    app.logger.addHandler(handler)
     app.config["JSON_AS_ASCII"] = False
-    #app.run(host="127.0.0.1", port="8080")
+    # 这样设置 host，只能在本机访问
+    #app.run(host="127.0.0.1", port="36818")
+    # 这样设置 host，可以允许局域网内的其他机器访问
     app.run(host="0.0.0.0", port="36818")
--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-python generate_poem.py os_model_ch_poem/pytorch_model.bin os_model_ch_poem/config.json os_model_ch_poem/vocab.txt 1
+#python generate_poem.py os_model_ch_poem/pytorch_model.bin os_model_ch_poem/config.json os_model_ch_poem/vocab.txt 1
+#python generate_poem.py model_jl_libai/'epoch=5-step=299.ckpt' os_model_ch_poem/config.json os_model_ch_poem/vocab.txt 1
+python generate_poem.py model_jl/'epoch=0-step=49999.ckpt' os_model_ch_poem/config.json os_model_ch_poem/vocab.txt 1
+# add delimeter in training data
+#python generate_poem.py model_jl_libai/'epoch=8-step=224.ckpt' os_model_ch_poem/config.json os_model_ch_poem/vocab.txt 1
+#python generate_poem.py model_jl/'epoch=0-step=49999.ckpt' os_model_ch_poem/config.json os_model_ch_poem/vocab.txt 1
